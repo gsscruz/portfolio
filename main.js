5bed5c818aa3c8dc32a879afb8f98922de2f87af
Gustavo <gmonte@tutanota.com>
2021-11-24 12:47:15 -0800

    Update

--- main.js
+++ main.js
@@ -18,18 +18,38 @@ function elevateNot(){
   linTranslate.classList.remove("rotate");
   gitTranslate.classList.remove("rotate");
 }
+
+function bringTextDown(){
+  linText.classList.add("bringTextDown");
+}
+
+function bringTextUp(){
+  linText.classList.remove("bringTextDown");
+}
+function bringTextDownGit(){
+  githubText.classList.add("bringTextDown");
+}
+
+function bringTextUpGit(){
+  githubText.classList.remove("bringTextDown");
+}
 // Call to action button
 const arrowButton = document.querySelector(".arrowCall");
+
 // Name at the header bar
 const lastName = document.querySelector("#lastN");
 const firstName = document.querySelector("#firstN");
+
 //icon set at the left side
 const iconsLeft = document.querySelector(".iconWrapper-left");
+
 //icon set at the right side
 const iconsRight = document.querySelector(".iconWrapper-right");
+
 //contact icons
 const linkedIn = document.querySelector(".linkedinIcon");
 const github = document.querySelector(".githubIcon");
+
 //imgs to be affected by the hover effects hover
 const linTranslate = document.getElementById("linked");
 const gitTranslate = document.getElementById("gitImg");
@@ -38,15 +58,27 @@ const gitTranslate = document.getElementById("gitImg");
 const linContainer = document.querySelector(".linkedinContainer");
 const gitContainer = document.querySelector(".githubContainer");
 
+//target Linkedin Text
+const linText = document.querySelector(".linkedinText");
+const githubText = document.querySelector(".githubText");
 
 //Slide name - reveal first name
 lastName.addEventListener("mouseover", nameSlider);
 
 //Fade in icons
 arrowButton.addEventListener("click", fadeInIcons);
+
 //rotate and elevate hovers
 linContainer.addEventListener("mouseover", elevateLin);
 linContainer.addEventListener("mouseout", elevateNot);
-
 gitContainer.addEventListener("mouseover", elevateGit);
 gitContainer.addEventListener("mouseout", elevateNot);
+
+
+//bring text down
+linContainer.addEventListener("mouseover",bringTextDown);
+linContainer.addEventListener("mouseout",bringTextUp);
+
+gitContainer.addEventListener("mouseover",bringTextDownGit);
+gitContainer.addEventListener("mouseout",bringTextUpGit);
+
